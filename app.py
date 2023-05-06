41a19be5789baaa20f94920eb6b49f47cf2cb691
Will Yelverton <william.yelverton@hotmail.com>
2023-05-06 21:26:40 +0100

    Added config and search type (rental/buy)

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
+import json
+from typing import Tuple
+
 from database.update_database import ManageDatabase
 from scraper.rightmove import RightMoveScraper
 from scraper_email.send_email import SendEmail
 
-# TODO: Put all this in config
+
+def load_config(config_file: str) -> Tuple[str, str, str]:
+    with open(config_file) as f:
+        config = json.load(f)
+        url = config.get('url', '')
+        table_name = config.get('table_name', '')
+        search_type = config.get('search_type', '')
+
+    return url, table_name, search_type
+
+URL, DATABASE_TABLE, SEARCH_TYPE = load_config('scraper/scraper_config.json')
+
 manage_database = ManageDatabase("database/rightmove")
-
-rightmove_scraper = RightMoveScraper(
-    'https://www.rightmove.co.uk/api/_search?locationIdentifier=STATION%5E2162&minBedrooms=1&maxPrice=1750'\
-        '&numberOfPropertiesPerPage=24&radius=3.0&sortType=6&index=0&includeLetAgreed=false&viewType=LIST&'\
-            'channel=RENT&areaSizeUnit=sqft&currencyCode=GBP&isFetching=false&viewport='
-)
-
+rightmove_scraper = RightMoveScraper(URL)
+# TODO: move this to a seperate app file as it is not part of the scraper
 send_email = SendEmail('scraper_email/gmail_info.json','Rightmove')
-
-DATABASE_TABLE = 'Clapham3Mile'
--- database/update_database.py
+++ database/update_database.py
@@ -7,7 +7,7 @@ class ManageDatabase:
 
     def create_db(self):
         sqlite3.connect(f"{self._db_name}.db")
-    
+
     def _connect_db(self):
         connection_obj = sqlite3.connect(f"{self._db_name}.db")
         cursor = connection_obj.cursor()
@@ -38,15 +38,8 @@ class ManageDatabase:
                 f" VALUES (:id, :price, :beds, :link, :address, :description, :image);"),
                 house
             )
-        # new_dict = {'id': 125447882, 'price': 1300, 'link': 'https://www.rightmove.co.uk/properties/125447882#/?channel=RES_LET', 'beds': 1, 'address': 'Wavertree Road, Streatham', 'description': 'An attractive first floor conversion flat which has an open plan kitchen-reception room benefitting from a vaulted ceiling and feature fireplace, a good sized double bedroom and modern bathroom.', 'image': 'https://media.rightmove.co.uk:443/dir/crop/10:9-16:9/53k/52415/125447882/52415_29788708_IMG_00_0000_max_476x317.jpeg'}
-        # cursor.execute(
-        #     (f"INSERT INTO {table_name} "
-        #         f"(ID, PRICE, BEDS, LINK, ADDRESS, DESCRIPTION, IMAGE)"
-        #         f" VALUES (:id, :price, :beds, :link, :address, :description, :image);"),
-        #         new_dict
-        # )
         connection_obj.commit()
-    
+
     def get_record_n_hours(self, table_name, hour=24)-> List:
         _, cursor = self._connect_db()
 
@@ -55,7 +48,7 @@ class ManageDatabase:
             WHERE TIMESTAMP >= datetime('now', '-{hour} hours') AND TIMESTAMP < datetime('now');"""
         )
         return cursor.fetchall()
-    
+
     def get_record(self, table_name, house_id):
         _, cursor = self._connect_db()
 
@@ -64,4 +57,3 @@ class ManageDatabase:
             WHERE ID={house_id} ORDER BY TIMESTAMP DESC;"""
         )
         return cursor.fetchall()
-
